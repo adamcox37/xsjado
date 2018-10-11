0ab60d01ccf4dff4fbcd782410af7ec65ac47c94
Adam Cox <adam_mcox@yahoo.com>
2018-10-11 19:16:27 -0400

    fixed slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,3 @@
-// Nav Dropdown //
-
-// When the user clicks on the button, toggle between hiding and showing the dropdown content
-f/*unction myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick= function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns= document.getElementById("dropdown-content");
-    var i;
-    for (i= 0; i < dropdowns.length; i++) {
-      var openDropdown= dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-// SHRINKING NAV BAR //
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "40px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "70px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-}
-
-//  STICKY NAV BAR //
-
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}*/
-
 // MANUAL SLIDESHOW //
 
 var slideIndex = 1;
@@ -64,7 +8,7 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Image controls
+// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
